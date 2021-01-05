d04a44651c351675a34663f5a45022b5151075bc
Juliette <juliette.bois@infomaniak.com>
2021-01-05 10:16:16 +0100

    Use data Fixtures for articles

--- www/src/Controller/HomeController.php
+++ www/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,16 +14,27 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $articles = $repo->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController'
+            'controller_name' => 'HomeController',
+            'articles' => $articles,
         ]);
     }
 
     /**
-     * @Route("/article/12", name="article_12")
+     * @Route("/article/{id}", name="article_12")
+     * @param $id
+     * @return Response
      */
-    public function showArticle(): Response
+    public function showArticle($id): Response
     {
-        return $this->render('home/article.html.twig');
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $article = $repo->find($id);
+
+        return $this->render('home/article.html.twig', [
+            'article' => $article,
+        ]);
     }
 }
