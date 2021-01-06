732a78ace910438e8eebb112ce8394a321aaad95
Juliette <juliette.bois@infomaniak.com>
2021-01-06 10:36:51 +0100

    Create form to create an article

--- www/src/Controller/CreateArticleController.php
+++ www/src/Controller/CreateArticleController.php
@@ -2,19 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CreateArticleController extends AbstractController
 {
     /**
-     * @Route("/create/article", name="create_article")
+     * @Route("/new/article", name="new_article")
      */
     public function index(): Response
     {
+        $article = new Article();
+        $form = $this->createFormBuilder($article)
+                    ->add('title')
+                    ->add('content')
+                    ->add('image')
+                    ->getForm();
+
         return $this->render('create_article/index.html.twig', [
-            'controller_name' => 'CreateArticleController',
+            'form_article' => $form->createView()
         ]);
     }
 }
