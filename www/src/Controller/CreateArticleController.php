98f2e335ea5e1ab8619eba378551315a99aceff4
Juliette <juliette.bois@infomaniak.com>
2021-01-06 10:45:56 +0100

    Form validation

--- www/src/Controller/CreateArticleController.php
+++ www/src/Controller/CreateArticleController.php
@@ -3,11 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,8 +17,11 @@ class CreateArticleController extends AbstractController
 {
     /**
      * @Route("/new/article", name="new_article")
+     * @param Request $request
+     * @param ObjectManager $manager
+     * @return Response
      */
-    public function index(): Response
+    public function index(Request $request, ObjectManager $manager): Response
     {
         $article = new Article();
         $form = $this->createFormBuilder($article)
@@ -24,6 +29,17 @@ class CreateArticleController extends AbstractController
                     ->add('content')
                     ->add('image')
                     ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article->setCreatedAt(new \DateTime());
+
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('article_show', ['id' => $article->getId()]);
+        }
 
         return $this->render('create_article/index.html.twig', [
             'form_article' => $form->createView()
